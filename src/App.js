b8674621742dfc032bc8a2564254a59636ec526f
Krecik <bartoszkraczek2006@gmail.com>
2023-09-26 19:14:50 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './globalStyle';
+import { lightTheme } from './components/themes';
+import { Route, Switch } from 'react-router-dom'; // Import Switch and Route from react-router-dom
+
 function App() {
-  return <div>
-    App File
-    </div>
-    
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={lightTheme}>
+        <Switch>
+          <Route></Route> {/* You should specify your routes here */}
+        </Switch>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
-
+export default App;
