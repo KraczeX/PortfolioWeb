d6e56a675644e8755e12997bc3ad285a3296d605
Krecik <bartoszkraczek2006@gmail.com>
2023-09-27 23:01:48 +0200

    update

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import PowerButton from "../subComponents/powerButton";
 import LogoComponent from "../subComponents/logoComponent";
 import SocialIcons from "../subComponents/socialIcons";
+import { NavLink } from "react-router-dom";
 
 
 const MainContainer = styled.div`
@@ -21,6 +22,60 @@ const MainContainer = styled.div`
 const Container = styled.div`
     padding: 2rem;
 `
+const Contact = styled(NavLink)`
+    color: ${props => props.theme.text};
+    position: absolute;
+    top: 2rem;
+    right: calc(1rem + 2vw);
+    text-decoration: none;
+    z-index: 1;
+    
+`
+
+const Soon = styled(NavLink)`
+    color: ${props => props.theme.text};
+    position: absolute;
+    top: 50%;
+    right: calc(1rem + 2vw);
+    transform: rotate(90deg) translate(-50%, -50%);
+    text-decoration: none;
+    z-index: 1;
+    
+`
+const Work = styled(NavLink)`
+    color: ${props => props.theme.text};
+    position: absolute;
+    top: 50%;
+    left: 2rem;
+    transform: translate(-50%, -50%) rotate(-90deg);
+    text-decoration: none;
+    z-index: 1;
+    
+`
+
+const BottomBar = styled.div`
+    position: absolute;
+    bottom: 1rem;
+    left: 0;
+    right: 0;
+    width: 100%;
+
+    display: flex;
+    justify-content: space-evenly;
+`
+
+const About = styled(NavLink)`
+    color: ${props => props.theme.text};
+    text-decoration: none;
+    z-index: 1;
+    
+`
+const Skills = styled(NavLink)`
+    color: ${props => props.theme.text};
+    text-decoration: none;
+    z-index: 1;
+    
+`
 
 
 
@@ -28,9 +83,43 @@ const Main = () => {
     return (
         <MainContainer>
             <Container>
-            <PowerButton/>
-            <LogoComponent/>
-            <SocialIcons/>
+                <PowerButton />
+                <LogoComponent />
+                <SocialIcons />
+
+                <Contact target="_blank" to="https://discord.gg/yJfcEqPvnB">
+                    <h2>
+                        Send Message
+                    </h2>
+                </Contact>
+
+                <Soon to="/">
+                    <h2>
+                        Soon
+                    </h2>
+                </Soon>
+
+                <Work to="/work">
+                    <h2>
+                        Work
+                    </h2>
+                </Work>
+
+
+                <BottomBar>
+                    <About to="/about">
+                        <h2>
+                            About
+                        </h2>
+                    </About>
+
+                    <Skills to="/skills">
+                        <h2>
+                            My Skills
+                        </h2>
+                    </Skills>
+                </BottomBar>
+
             </Container>
         </MainContainer>
     )
