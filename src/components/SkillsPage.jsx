7f221a1abfccd888cbd2d2defa695448171a295c
Krecik <bartoszkraczek2006@gmail.com>
2023-10-04 11:14:34 +0200

    update

--- src/components/SkillsPage.jsx
+++ src/components/SkillsPage.jsx
@@ -3,6 +3,10 @@ import styled, { ThemeProvider } from "styled-components";
 import { lightTheme } from "./themes";
 import { Design, Develope } from './AllSvgs';
 
+import PowerButton from "../subComponents/powerButton";
+import LogoComponent from "../subComponents/logoComponent";
+import SocialIcons from "../subComponents/socialIcons";
+
 const Box = styled.div`
     background-color: ${props => props.theme.body};
     width: 100vw;
@@ -11,7 +15,7 @@ const Box = styled.div`
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-`
+`;
 
 const Main = styled.div`
     border: 2px solid ${props => props.theme.text};
@@ -22,39 +26,89 @@ const Main = styled.div`
     height: 60vh;
     z-index: 3;
     line-height: 1.5;
+    cursor: pointer;
 
     font-family: 'Ubuntu mono', monospace;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-`
+
+
+    &:hover{
+        color: ${props => props.theme.body};
+        background-color: ${props => props.theme.text};
+    }
+`;
 
 const Title = styled.h2`
     display: flex;
     justify-content: center;
     align-items: center;
     font-size: calc(1em + 1vw);
-`
+    ${Main}:hover &{
+    &>*{
+        fill:${props => props.theme.body};
+    }
+}
+
+    &>*:first-child{
+    margin-right: 1rem;
+}
+`;
+
 const Description = styled.div`
     color: ${props => props.theme.text};
     font-size: calc(0.6em + 1vw);
     padding: 0.5rem 0;
-`
 
+    ${Main}:hover &{
+   
+   color:${props => props.theme.body};
+
+}
+
+    strong{
+        margin-bottom: 1rem;
+        text-transform: uppercase;
+    }
+    ul,p{
+        margin-left: 2rem;
+    }
+`;
 
 const Skills = () => {
     return (
         <ThemeProvider theme={lightTheme}>
             <Box>
+                <LogoComponent theme='light' />
+                <SocialIcons theme='light' />
+                <PowerButton />
+
                 <Main>
                     <Title>
                         <Develope width={40} height={40} /> Bot Developer
                     </Title>
                     <Description>
-                    I love to create design which speaks, Keep it clean, minimal and simple.
+                        I love to create designs that speak, keeping them clean, minimal, and simple.
                     </Description>
                     <Description>
-                   <strong>I like to design</strong>
+                        <strong>I like to Design</strong>
+                        <ul>
+                            <li>
+                                Web Design
+                            </li>
+                            <li>
+                                Mobile Apps
+                            </li>
+                        </ul>
+                    </Description>
+                    <Description>
+                        <strong>Tools</strong>
+                        <ul>
+                            <li>
+                                Figma
+                            </li>
+                        </ul>
                     </Description>
                 </Main>
                 <Main>
@@ -62,12 +116,24 @@ const Skills = () => {
                         <Design width={40} height={40} /> Frontend Developer
                     </Title>
                     <Description>
-                    I value business or brand for which i'm creating, thus i enjoy bringing new ideas to life.
+                        I value the business or brand for which I'm creating, thus I enjoy bringing new ideas to life.
+                    </Description>
+                    <Description>
+                        <strong>Skills</strong>
+                        <p>
+                            Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase etc.
+                        </p>
+                    </Description>
+                    <Description>
+                        <strong>Tools</strong>
+                        <p>
+                            VScode, Github, Codepen etc.
+                        </p>
                     </Description>
                 </Main>
             </Box>
         </ThemeProvider>
-    )
-}
+    );
+};
 
 export default Skills;
