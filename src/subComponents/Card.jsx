e4bf57d9ea3fe426bdeb9b617983f638ac719f46
Krecik <bartoszkraczek2006@gmail.com>
2023-10-05 11:16:09 +0200

    update

--- src/subComponents/Card.jsx
+++ src/subComponents/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 
@@ -38,12 +39,27 @@ const Tags = styled.div`
     padding-top: 0.5rem;
     display: flex;
     flex-wrap: wrap;
+    ${Box}:hover &{
+        border-top: 2px solid ${props => props.theme.text}
+    }
 `
 
 const Tag = styled.span`
     margin-right: 1rem;
+    font-size: calc(0.8em + 0.3vw);
 `
 
+const Footer = styled.footer`
+    display: flex;
+    justify-content: space-between;
+`
+
+const Link = styled(NavLink)`
+    background-color: ${props => props.theme.body};
+    color: ${props => props.theme.text};
+    text-decoration: none;
+    padding: 0.5rem calc(2rem + 2vw);
+`
 const Card = (props) => {
 
     const {id, name, description, tags, demo, github} = props.data;
@@ -61,6 +77,12 @@ const Card = (props) => {
                     })
                 }
             </Tags>
+            <Footer>
+                <Link to={{pathname: `${demo}`}} target="_blank">
+                    Visit
+                </Link>
+
+            </Footer>
         </Box>
     )
 }
